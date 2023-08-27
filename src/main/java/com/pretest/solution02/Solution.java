aa18a32fcac86a28d583360dcaa235e8628b64eb
gangdodan <dodannnn4@gmail.com>
2023-08-27 13:05:29 +0900

    Feat: Solution02 구현

---
+++ src/main/java/com/pretest/solution02/Solution.java
@@ -0,0 +1,36 @@
+package com.pretest.solution02;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+    static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int[] coins, dp;
+    static int amount;
+
+    public static int countCoinSum() {
+        dp[0] = 1;
+        for (int coin : coins) {
+            for (int i = coin; i <= amount; i++) {
+                dp[i] += dp[i - coin];
+            }
+        }
+        return dp[amount];
+    }
+
+    public static void main(String[] args) throws IOException {
+        amount = Integer.parseInt(br.readLine());
+        String input = br.readLine();
+        String[] arr = input.split(" ");
+
+        dp = new int[amount + 1];
+        coins = new int[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            coins[i] = Integer.parseInt(arr[i]);
+        }
+
+        System.out.println("result = " + countCoinSum());
+    }
+}
